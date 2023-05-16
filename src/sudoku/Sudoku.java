755aafe51e4ad5a926b55f519e387c3d7a0ae252
Strykr <63495737+Strykr69@users.noreply.github.com>
2023-05-16 21:46:38 +0200

    so ein Schmarn

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -9,39 +9,34 @@ public class Sudoku {
     public Sudoku(){
     }
 
-    public int getDimension() {
-        return dimension;
-    }
-
     public Field getField(int x, int y) {
         return board[x][y];
     }
 
     @Override
     public String toString() {
-        for(int o = 0; o < dimension; o++){
+        for(int o = 0; o < 9; o++){
             StringBuilder s = new StringBuilder();
-            for(int p = 0; p < dimension; p++){
+            for(int p = 0; p < 9; p++){
                 s.append(board[o][p].toString());
                 s.append(" ");
-                if((p)%3==2 && p != dimension-1){
+                if((p)%3==2 && p != 9-1){
                     s.append("| ");
                 }
             }
             System.out.println(s);
-            if((o)%3==2 && o != dimension-1) System.out.println("------+-------+------");
+            if((o)%3==2 && o != 9-1) System.out.println("------+-------+------");
         }
         return ("");
     }
 
     public void initialize(int... values){
-        if(sqrt(values.length)%1!=0) throw new RuntimeException("BOARD IS NOT SYMMETRICAL - CHECK INPUT");
-        this.dimension = (int) sqrt(values.length);
-        this.board = new Field[dimension][dimension];
-        for(int j = 0; j < dimension; j++){
-            for(int k = 0; k < dimension; k++){
+        if(sqrt(values.length)!=9) throw new RuntimeException("BOARD DOES NOT CONTAIN ENOUGH INTEGERS");
+        this.board = new Field[9][9];
+        for(int j = 0; j < 9; j++){
+            for(int k = 0; k < 9; k++){
                 Field tempField = new Field(k,j);
-                tempField.setValue(Value.of(values[(j * dimension + k)]));
+                tempField.setValue(Value.of(values[(j * 9 + k)]));
                 board[j][k] = tempField;
             }
         }
