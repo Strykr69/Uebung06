9d175d6fc44a46ca7036c6a338fa77d88b0487c6
Strykr <63495737+Strykr69@users.noreply.github.com>
2023-05-16 21:50:20 +0200

    so ein Schmarn

--- src/sudoku/SudokuApp.java
+++ src/sudoku/SudokuApp.java
@@ -3,15 +3,6 @@ package sudoku;
 
 public class SudokuApp {
     public static void main(String[] args){
-//        String a = Value.EIGHT.toString(); //   "8"
-//        String b = Value.EIGHT.name();     //   EIGHT
-//        int c = Value.EIGHT.id;            //   8
-//        System.out.println(a);
-//        System.out.println(b);
-//        System.out.println(c);
-//        Value d = Value.of(9);
-//        System.out.println(d.name());
-
         final Sudoku board = new Sudoku();
         board.initialize(4, 5, 0, 0, 0, 0, 2, 0, 0, 6, 0, 0, 0, 2, 4, 8, 0, 0, 8, 0, 0, 0, 6, 1, 3, 0, 0, 0, 9, 0, 4, 0, 0, 0, 5, 0, 0, 1, 0, 2, 0, 8, 0, 7, 0, 0, 3, 0, 0, 0, 9, 0, 8, 0, 0, 0, 7, 1, 4, 0, 0, 0, 8, 0, 0, 2, 7, 9, 0, 0, 0, 6, 0, 0, 5, 0, 0, 0, 0, 2, 1);
         System.out.println(board);
@@ -27,7 +18,6 @@ public class SudokuApp {
         SEVEN(7,"7"),
         EIGHT(8,"8"),
         NINE(9,"9");
-//        EMPTY(0,".");
 
         public final int id;
         public final String label;
